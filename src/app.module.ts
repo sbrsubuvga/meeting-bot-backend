6f6b62d0f1bbad6a299762cfd01f632f10765e61
subair <subair@lithos.in>
2025-06-04 13:07:46 +0530

    >>>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { MeetingService } from './meeting/meeting.service';
 import { RecorderService } from './recorder/recorder.service';
 import { ChatGptService } from './summarizer/chatgpt.service';
