cb27723380e129d0473c4c7edb28edf26f6d7598
subair <subair@lithos.in>
2025-06-05 10:53:25 +0530

    >>>

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  console.log('Server is running on http://localhost:3000');
+  await app.listen(process.env.PORT ?? 9000);
+  console.log('Server is running on http://localhost:9000');
 }
 bootstrap();
 
